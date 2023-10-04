0ff0765fc4cd4d1a4f8f043cba81c3c513705447
sikder <shbadhon10@gmail.com>
2023-10-05 03:40:29 +0600

    [+] Some Modification for lspsaga

--- lua/base46/integrations/lspsaga.lua
+++ lua/base46/integrations/lspsaga.lua
@@ -3,8 +3,12 @@ local colors = require("base46").get_theme_tb "base_30"
 return {
   SagaBorder = {
     bg = colors.darker_black,
+    fg = colors.folder_bg,
   },
   SagaNormal = {
     bg = colors.darker_black,
   },
+  DiffAdd = {
+    fg = colors.cyan,
+  }
 }
